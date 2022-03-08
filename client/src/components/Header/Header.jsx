259915729f64970e53a98763437dfaac9e8ad45c
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-03-08 11:32:03 +0900

    edit code

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,77 +7,70 @@ import { Container } from "reactstrap";
 import { NavLink, Link } from "react-router-dom";
 
 const NAV__LINKS = [
-  {
-    display: "Home",
-    url: "/home",
-  },
-  {
-    display: "Market",
-    url: "/market",
-  },
-  {
-    display: "Create",
-    url: "/create",
-  },
-  {
-    display: "Game",
-    url: "/game",
-  },
-  {
-    display: "Contact",
-    url: "/contact",
-  },
+    {
+        display: "Home",
+        url: "/",
+    },
+    {
+        display: "Market",
+        url: "/market",
+    },
+    {
+        display: "Create",
+        url: "/create",
+    },
+    {
+        display: "Game",
+        url: "/game",
+    },
+    {
+        display: "Contact",
+        url: "/contact",
+    },
 ];
 
 const Header = () => {
-  return (
-    <header className="header">
-      <Container>
-        <div className="navigation">
-          <div className="logo">
-            <h2 className="d-flex gap-2 align-items-center">
-              <span>
-                <i className="ri-bear-smile-line">
-                  {/* <img src={logo} alt="loading..." /> */}
-                </i>
-              </span>
-              NFTs
-            </h2>
-          </div>
+    return (
+        <header className="header">
+            <Container>
+                <div className="navigation">
+                    <div className="logo">
+                        <h2 className="d-flex gap-2 align-items-center">
+                            <span>
+                                <i className="ri-bear-smile-line">{/* <img src={logo} alt="loading..." /> */}</i>
+                            </span>
+                            NFTs
+                        </h2>
+                    </div>
 
-          <div className="nav__menu">
-            <ul className="nav__list">
-              {NAV__LINKS.map((item, index) => (
-                <li className="nav__item" key={index}>
-                  <NavLink
-                    to={item.url}
-                    className={(navClass) =>
-                      navClass.isActive ? "active" : ""
-                    }
-                  >
-                    {item.display}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            {NAV__LINKS.map((item, index) => (
+                                <li className="nav__item" key={index}>
+                                    <NavLink to={item.url} className={(navClass) => (navClass.isActive ? "active" : "")}>
+                                        {item.display}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-          <div className="nav__right d-flex align-items-center gap-5">
-            <button className="btn d-flex gap-2 align-items-center">
-              <span>
-                <i className="ri-wallet-line"></i>
-              </span>
-              <Link to="/wallet">Connect Wallet</Link>
-            </button>
+                    <div className="nav__right d-flex align-items-center gap-5">
+                        <button className="btn d-flex gap-2 align-items-center">
+                            <span>
+                                <i className="ri-wallet-line"></i>
+                            </span>
+                            <Link to="/wallet">Connect Wallet</Link>
+                        </button>
 
-            <span className="mobile__menu">
-              <i className="ri-menu-line"></i>
-            </span>
-          </div>
-        </div>
-      </Container>
-    </header>
-  );
+                        <span className="mobile__menu">
+                            <i className="ri-menu-line"></i>
+                        </span>
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
 };
 
 export default Header;
