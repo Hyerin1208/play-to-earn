459e104aa41b17f0a25bac8d6fa5d41f9c5d43ff
이성현 <marvel97@naver.com>
2022-03-09 12:26:48 +0900

    SnakeGame Update

--- client/src/components/routes/Routers.js
+++ client/src/components/routes/Routers.js
@@ -5,12 +5,13 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Market from "../pages/Market";
 import Create from "../pages/Contact";
-import Game from "../pages/Game";
 import Contact from "../pages/Contact";
 import SellerProfile from "../pages/SellerProfile";
 import EditProfile from "../pages/EditProfile";
 import Wallet from "../pages/Wallet";
 import NftDetails from "../pages/NftDetails";
+import SnakeGame from "../pages/SnakeGame/SnakeGame";
+// import Game from "../pages/Game";
 
 const Routers = () => {
     return (
@@ -18,12 +19,13 @@ const Routers = () => {
             <Route exact path="/" element={<Home />} />
             <Route path="/market" element={<Market />} />
             <Route path="/create" element={<Create />} />
-            <Route path="/game" element={<Game />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/edit-profile" element={<EditProfile />} />
             <Route path="/seller-profile" element={<SellerProfile />} />
             <Route path="/wallet" element={<Wallet />} />
             <Route path="/market/:id" element={<NftDetails />} />
+            <Route path="/game" element={<SnakeGame />} />
+            {/* <Route path="/game" element={<Game />} /> */}
         </Routes>
     );
 };
