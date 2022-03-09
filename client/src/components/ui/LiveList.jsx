f663125698fe5029da99d09fad8370817ca55e9e
Hyerin1208 <fghjiff8@gmail.com>
2022-03-09 17:08:07 +0900

    Top collections - main list

--- client/src/components/ui/LiveList.jsx
+++ client/src/components/ui/LiveList.jsx
@@ -3,15 +3,15 @@ import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./live-list.css";
 
-import img01 from "../../assets/images/q1.png";
-import admin from "../../assets/images/avatar.png";
+import NftCard from "./NftCard";
+import { NFT__DATA } from "../../assets/data/data.js";
 
 const LiveList = () => {
   return (
     <section>
       <Container>
         <Row>
-          <Col lg="12" className="mb-4">
+          <Col lg="12" className="mb-5">
             <div className="live__list__top">
               <h3>Top collections over last 7 days</h3>
               <span>
@@ -20,46 +20,11 @@ const LiveList = () => {
             </div>
           </Col>
 
-          <Col lg="3">
-            <div className="single__nft__card">
-              <div className="nft__img">
-                <img src={img01} alt="" />
-              </div>
-
-              <div className="nft__content">
-                <h5 className="nft__title">Stitch Club</h5>
-
-                <div className="creator__info-wrapper">
-                  <div className="creator__img">
-                    <img src={admin} alt="" />
-                  </div>
-                  <div className="creator__info">
-                    <div className="creator">
-                      <h6>Created By</h6>
-                      <p>ALTAVA Group</p>
-                    </div>
-
-                    <div className="bid">
-                      <h6>Current Bid</h6>
-                      {/* 우리만의 토큰이름을 정해서 아래단위 바꾸기 */}
-                      <p>4.89 EHT</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="bid__box">
-                  <button className="bid__btn">
-                    <i className="ri-shopping-bag-line"></i>
-                    Place Bid
-                  </button>
-
-                  <span className="view__link">
-                    <Link to="#">View More</Link>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </Col>
+          {NFT__DATA.slice(0, 4).map((item, index) => (
+            <Col lg="3" key={index}>
+              <NftCard key={item.id} item={item} />
+            </Col>
+          ))}
         </Row>
       </Container>
     </section>
