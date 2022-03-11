ee37cc04fe4ba5baef46ebb9e0ff0c6f1ca036df
Hyerin1208 <fghjiff8@gmail.com>
2022-03-11 18:30:07 +0900

    edit game page layout

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,26 +1,32 @@
 import React, { Fragment } from "react";
 import { Link, Routes, Route } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
 import SnakeGames from "../pages/SnakeGame/SnakeGame";
 import TetrisGames from "../pages/TetrisGame/Tetris.js";
+import CommonSection from "../ui/CommonSection";
 
 function Game() {
-    return (
-        <>
-            <h1>게임모음집</h1>
-            <ul>
-                <li>
-                    <Link to="SnakeGames">스네이크</Link>
-                </li>
-                <li>
-                    <Link to="TetrisGames">테트리스</Link>
-                </li>
-            </ul>
-            <Routes>
-                <Route path="SnakeGames" element={<SnakeGames />} />
-                <Route path="TetrisGames" element={<TetrisGames />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <CommonSection title="GAME'S FEATURES" />
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className="single__game__card">
+              <Link to="SnakeGames">스네이크</Link>
+            </div>
+            <div className="single__game__card">
+              <Link to="TetrisGames">테트리스</Link>
+            </div>
+          </Col>
+        </Row>
+        <Routes>
+          <Route path="SnakeGames" element={<SnakeGames />} />
+          <Route path="TetrisGames" element={<TetrisGames />} />
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
 export default Game;
