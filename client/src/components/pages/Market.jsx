335aed5fe2d92b2b2d6feef80871e11747e1e347
Hyerin1208 <fghjiff8@gmail.com>
2022-03-11 11:14:54 +0900

    nft market ui 구현중

--- client/src/components/pages/Market.jsx
+++ client/src/components/pages/Market.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
+import CommonSection from "../ui/CommonSection";
+import NftCard from "../ui/NftCard";
+
+import { NFT__DATA } from "../../assets/data/data";
+
+import { Container, Row, Col } from "reactstrap";
+
+import "./market.css";
+
 const Market = () => {
-  return <div>Market</div>;
+  const handleCategory = () => {};
+
+  const handleItems = () => {};
+
+  const handleSort = () => {};
+
+  return (
+    <>
+      <CommonSection title={"MarketPlacae"} />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <div className="market__product__filter">
+                <div className="filter__left">
+                  <div className="all__category__filter">
+                    <select onChange={handleCategory}>
+                      <option>All Categories</option>
+                      <option value="Art">Art</option>
+                      <option value="badge">Badge</option>
+                      <option value="Item">Item</option>
+                      <option value="domain-name">Domain Name</option>
+                      <option value="Trading Card">Trading Card</option>
+                    </select>
+                  </div>
+                  <div className="all__items__filter">
+                    <select onChange={handleItems}>
+                      <option value="">All Items</option>
+                      <option value="single-item">Single Item</option>
+                      <option value="bundle">Bundle</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="filter__right">
+                  <select onChange={handleSort}>
+                    <option>Sort By</option>
+                    <option value="high">High Rate</option>
+                    <option value="mid">Mid Rate</option>
+                    <option value="low">Low Rate</option>
+                  </select>
+                </div>
+              </div>
+            </Col>
+
+            {NFT__DATA.map((item) => (
+              <Col lg="3" md="4" sm="6" className="mb-4" key={item.id}>
+                <NftCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Market;
