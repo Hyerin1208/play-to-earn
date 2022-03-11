43c0c2c3b3c506da6e9651eb604ac5bb49f43729
Hyerin1208 <fghjiff8@gmail.com>
2022-03-11 20:19:49 +0900

    create nft

--- client/src/components/pages/Create.jsx
+++ client/src/components/pages/Create.jsx
@@ -27,8 +27,60 @@ const Create = () => {
         <Container>
           <Row>
             <Col lg="3" md="4" sm="6">
-              <h5>Preview Item</h5>
+              <h5 className="preview__item">Preview Item</h5>
               <NftCard item={item} />
+            </Col>
+
+            <Col lg="9" md="8" sm="6">
+              <div className="create__input">
+                <form>
+                  <div className="form__input">
+                    <label htmlFor="">Upload File</label>
+                    <input type="file" className="upload__input" />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Price</label>
+                    <input
+                      type="number"
+                      placeholder="Enter price for one item (ETH)"
+                    />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Minimum Bid</label>
+                    <input type="number" placeholder="Enter minimum bid" />
+                  </div>
+
+                  <div className="date__form">
+                    <div className="form__input w-50">
+                      <label htmlFor="">Starting Date</label>
+                      <input type="date" />
+                    </div>
+
+                    <div className="form__input w-50">
+                      <label htmlFor="">Expiration Date</label>
+                      <input type="date" />
+                    </div>
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Title</label>
+                    <input type="text" placeholder="Enter title" />
+                  </div>
+
+                  <div className="form__input">
+                    <label htmlFor="">Description</label>
+                    <textarea
+                      name=""
+                      id=""
+                      rows="10"
+                      placeholder="Enter Description"
+                      className="w-100"
+                    ></textarea>
+                  </div>
+                </form>
+              </div>
             </Col>
           </Row>
         </Container>
