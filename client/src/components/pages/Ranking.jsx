36fca143d590db29bb4b9ea5133d4a5fae06d647
Hyerin1208 <fghjiff8@gmail.com>
2022-03-12 23:47:37 +0900

    순위페이지 ui

--- client/src/components/pages/Ranking.jsx
+++ client/src/components/pages/Ranking.jsx
@@ -1,8 +1,79 @@
 import { useState } from "react";
+import { Col, Container } from "reactstrap";
+import CommonSection from "../ui/CommonSection";
 import "./ranking.css";
 
 const Ranking = () => {
-  return <div>Ranking</div>;
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+    <>
+      <CommonSection title="Ranking" />
+      <div className="Ranking__container">
+        <div className="bloc-tabs">
+          <button
+            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            종합랭킹
+          </button>
+          <button
+            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+            주간랭킹
+          </button>
+          <button
+            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+            onClick={() => toggleTab(3)}
+          >
+            MY랭킹
+          </button>
+        </div>
+
+        <div className="content-tabs">
+          <div
+            className={
+              toggleState === 1 ? "content  active-content" : "content"
+            }
+          >
+            <h2>종합랭킹</h2>
+            <hr />
+            <Container>
+              <div className="ranking__box">여기에 종합랭킹 순위표만들기</div>
+            </Container>
+          </div>
+
+          <div
+            className={
+              toggleState === 2 ? "content  active-content" : "content"
+            }
+          >
+            <h2>주간랭킹</h2>
+            <hr />
+            <Container>
+              <div className="ranking__box">여기에 주간랭킹 순위표만들기</div>
+            </Container>
+          </div>
+
+          <div
+            className={
+              toggleState === 3 ? "content  active-content" : "content"
+            }
+          >
+            <h2>나의랭킹</h2>
+            <hr />
+            <Container>
+              <p>여기에 나의랭킹 페이지만들기</p>
+            </Container>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Ranking;
