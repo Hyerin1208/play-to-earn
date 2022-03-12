23b4be3d07e793e2c6a91fbf8ca49b888b04650d
Hyerin1208 <fghjiff8@gmail.com>
2022-03-13 00:51:42 +0900

    add ranking btn + compensation btn page layout OK

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -1,21 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import { Col, Container, Row } from "reactstrap";
 import SnakeGames from "../pages/SnakeGame/SnakeGame";
 import TetrisGames from "../pages/TetrisGame/Tetris.js";
 import CommonSection from "../ui/CommonSection";
-
 import Ranking from "./Ranking";
 
+import "./game.css";
+import Compensation from "./Compensation";
+
 function Game() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <>
+    <Fragment>
       <CommonSection title="GAME'S FEATURES" />
       <Container>
-        <button className="ranking__btn">
-          <Link to="/ranking">Ranking</Link>
-        </button>
         <Row>
+          <Col>
+            <div className="game__infoBox">
+              <button className="ranking__btn">
+                <Link to="/ranking">랭킹 확인</Link>
+              </button>
+
+              <button
+                className="Compensation__btn"
+                onClick={() => setShowModal(true)}
+              >
+                랭킹별 보상리스트
+              </button>
+
+              {showModal && <Compensation setShowModal={setShowModal} />}
+            </div>
+          </Col>
+
           <Col lg="3" md="4" sm="6">
             <div className="single__game__card">
               <Link to="SnakeGames">스네이크</Link>
@@ -29,9 +47,10 @@ function Game() {
           <Route path="SnakeGames" element={<SnakeGames />} />
           <Route path="TetrisGames" element={<TetrisGames />} />
           <Route path="Ranking" element={<Ranking />} />
+          <Route path="Compensation" element={<Compensation />} />
         </Routes>
       </Container>
-    </>
+    </Fragment>
   );
 }
 
