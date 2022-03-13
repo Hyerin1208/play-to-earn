5ed89892a8fd0e2b4df579205ed130945f138b32
Hyerin1208 <fghjiff8@gmail.com>
2022-03-13 19:30:01 +0900

    airdrop + tokenchart

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -5,13 +5,17 @@ import MainSection from "../ui/MainSection";
 import LiveList from "../ui/LiveList";
 import Members from "../ui/Members";
 import Tutorial from "../ui/Tutorial";
+import TokenNomic from "../ui/TokenNomic";
+import AirDrop from "../ui/AirDrop";
 
 const Home = () => {
   return (
     <>
       <MainSection />
+      <AirDrop />
       <LiveList />
       <Tutorial />
+      <TokenNomic />
       <Members />
     </>
   );
