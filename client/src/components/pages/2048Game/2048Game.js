d92e99ec20e10f81327fddf7f542f46e2e0e61ae
이성현 <marvel97@naver.com>
2022-03-14 16:57:47 +0900

    Update 2048Game.js

--- client/src/components/pages/2048Game/2048Game.js
+++ client/src/components/pages/2048Game/2048Game.js
@@ -1,143 +1,120 @@
-import React, { Component, useState } from "react";
+import React, {Component} from "react";
 import "./2048Game.css";
 import Grid from "./Grid";
 import NumberTable from "./NumberTable";
 import Score from "./Score";
 import Hover from "./Hover";
 
-function Puzzle(props) {
-  //   constructor(props) {
-  //     super(props);
-  //     state = {
-  //       grid: new Grid(),
-  //       best: 0,
-  //       beginX: 0,
-  //       beginY: 0,
-  //       endX: 0,
-  //       endY: 0,
-  //       enableSwipe: false,
-  //     };
-  //   }
-
-  const [gird, setGrid] = useState(Grid());
-  const [best, setBset] = useState(0);
-  const [beginX, setBeginX] = useState(0);
-  const [beginY, setBeginY] = useState(0);
-  const [endX, setEndX] = useState(0);
-  const [endY, setEndY] = useState(0);
-  const [enableSwipe, setEnableSwipe] = useState(false);
-
-  const getPosDuringEvent = (e) => {
-    let X, Y;
-    if (e.touches) {
-      X = e.touches[0].screenX;
-      Y = e.touches[0].screenY;
-    } else {
-      X = e.screenX;
-      Y = e.screenY;
+class Puzzle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {grid: new Grid(), best: 0, beginX: 0, beginY: 0, endX: 0, endY: 0, enableSwipe: false};
     }
-    return [X, Y];
-  };
 
-  const componentDidMount = () => {
-    let events = {
-      keydown: onKeyPressed(),
-      touchstart: onTouchStart(),
-      mousedown: onTouchStart(),
-      touchmove: onTouchMove(),
-      touchend: onTouchEnd(),
-      mousemove: onTouchMove(),
-      mouseup: onTouchEnd(),
-      gesturestart: onGestureStart(),
-    };
-
-    for (let e in events) {
-      document.addEventListener(e, events[e]);
+    static getPosDuringEvent(e) {
+        let X, Y;
+        if (e.touches) {
+            X = e.touches[0].screenX;
+            Y = e.touches[0].screenY;
+        } else {
+            X = e.screenX;
+            Y = e.screenY;
+        }
+        return [X, Y];
     }
-  };
 
-  const onKeyPressed = (e) => {
-    let legalKey = false;
-    let grid = state.grid;
-    const actionMap = { 37: "left", 38: "up", 39: "right", 40: "down" };
-    if (e.keyCode >= 37 && e.keyCode <= 40) {
-      let action = actionMap[e.keyCode];
-      legalKey = grid.action(action);
+    componentDidMount() {
+        let events = {
+            "keydown": this.onKeyPressed,
+            "touchstart": this.onTouchStart,
+            "mousedown": this.onTouchStart,
+            "touchmove": this.onTouchMove,
+            "touchend": this.onTouchEnd,
+            "mousemove": this.onTouchMove,
+            "mouseup": this.onTouchEnd,
+            "gesturestart": this.onGestureStart,
+        };
+
+        for (let e in events) {
+            document.addEventListener(e, events[e].bind(this));
+        }
     }
-    if (legalKey) {
-      grid.addValue();
+
+    onKeyPressed(e) {
+        let legalKey = false;
+        let grid = this.state.grid;
+        const actionMap = {37: "left", 38: "up", 39: "right", 40: "down"};
+        if (e.keyCode >= 37 && e.keyCode <= 40) {
+            let action = actionMap[e.keyCode];
+            legalKey = grid.action(action);
+        }
+        if (legalKey) {
+            grid.addValue();
+        }
+        this.setState({grid: grid});
+        this.updateBest();
+        e.preventDefault();
     }
-    useState({ grid: grid });
-    updateBest();
-    e.preventDefault();
-  };
 
-  const onTouchStart = (e) => {
-    let [X, Y] = Puzzle.getPosDuringEvent(e);
-    useState({
-      beginX: X,
-      beginY: Y,
-      endX: X,
-      endY: Y,
-      enableSwipe: true,
-    });
-    e.preventDefault();
-  };
-
-  const onTouchMove = (e) => {
-    let [X, Y] = Puzzle.getPosDuringEvent(e);
-    setState({ endX: X, endY: Y });
-    e.preventDefault();
-  };
-
-  const onTouchEnd = (e) => {
-    let grid = state.grid;
-    let [beginX, beginY] = [state.beginX, state.beginY];
-    let [endX, endY] = [state.endX, state.endY];
-    if (
-      Math.pow(endX - beginX, 2) + Math.pow(endY - beginY, 2) > 10 &&
-      state.enableSwipe
-    ) {
-      let action = "";
-      if (Math.abs(beginX - endX) > Math.abs(beginY - endY)) {
-        action = beginX < endX ? "right" : "left";
-      } else {
-        action = beginY < endY ? "down" : "up";
-      }
-      if (grid.action(action)) {
-        grid.addValue();
-      }
+    onTouchStart(e) {
+        let [X, Y] = Puzzle.getPosDuringEvent(e);
+        this.setState({beginX: X, beginY: Y, endX: X, endY: Y, enableSwipe: true});
+        e.preventDefault();
     }
-    setState({ grid: grid, enableSwipe: false });
-    updateBest();
-    e.preventDefault();
-  };
 
-  const onGestureStart = (e) => {
-    setState({ enableSwipe: false });
-    e.preventDefault();
-  };
-
-  const restart = () => {
-    setState({ grid: new Grid() });
-  };
-
-  const updateBest = () => {
-    if (state.grid.score > state.best) {
-      setState({ best: state.grid.score });
+    onTouchMove(e) {
+        let [X, Y] = Puzzle.getPosDuringEvent(e);
+        this.setState({endX: X, endY: Y});
+        e.preventDefault();
     }
-  };
 
-  return (
-    <div>
-      <div id={"score-group"}>
-        <Score info={"SCORE"} score={state.grid.score} />
-        <Score info={"BEST"} score={state.best} />
-      </div>
-      <NumberTable numbers={state.grid.numbers} />
-      <Hover alive={state.grid.alive()} restart={restart} />
-    </div>
-  );
+    onTouchEnd(e) {
+        let grid = this.state.grid;
+        let [beginX, beginY] = [this.state.beginX, this.state.beginY];
+        let [endX, endY] = [this.state.endX, this.state.endY];
+        if (Math.pow(endX - beginX, 2) + Math.pow(endY - beginY, 2) > 10 && this.state.enableSwipe) {
+            let action = "";
+            if (Math.abs(beginX - endX) > Math.abs(beginY - endY)) {
+                action = beginX < endX ? "right" : "left";
+            } else {
+                action = beginY < endY ? "down" : "up";
+            }
+            if (grid.action(action)) {
+                grid.addValue();
+            }
+        }
+        this.setState({grid: grid, enableSwipe: false});
+        this.updateBest();
+        e.preventDefault();
+    }
+
+    onGestureStart(e) {
+        this.setState({enableSwipe: false});
+        e.preventDefault();
+    }
+
+    restart() {
+        this.setState({grid: new Grid()});
+    }
+
+    updateBest() {
+        if (this.state.grid.score > this.state.best) {
+            this.setState({best: this.state.grid.score});
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div id={"score-group"}>
+                    <Score info={"SCORE"} score={this.state.grid.score}/>
+                    <Score info={"BEST"} score={this.state.best}/>
+                </div>
+                <NumberTable numbers={this.state.grid.numbers}/>
+                <Hover alive={this.state.grid.alive()} restart={this.restart.bind(this)}/>
+            </div>
+        );
+    }
 }
 
 export default Puzzle;
