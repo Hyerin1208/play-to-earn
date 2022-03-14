0ab05856a7f05bae8b46bb0de86e8bdacf1b9b1e
이성현 <marvel97@naver.com>
2022-03-14 14:44:37 +0900

    2048game edit

--- client/src/components/pages/2048Game/NumberRow.js
+++ client/src/components/pages/2048Game/NumberRow.js
@@ -1,17 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./2048Game.css";
 import NumberCell from "./NumberCell";
 
 class NumberRow extends Component {
-    render() {
-        const numberRow = this.props.numberRow;
-        const numberRowContent = numberRow.map((number) =>
-            <NumberCell key={number.id} number={number}/>
-        );
-        return (
-            <div className="number-row">{numberRowContent}</div>
-        );
-    }
+  render() {
+    const numberRow = this.props.numberRow;
+    const numberRowContent = numberRow.map((number, index) => (
+      <NumberCell key={index} number={number} />
+    ));
+    return <div className="number-row">{numberRowContent}</div>;
+  }
 }
 
 export default NumberRow;
--- client/src/components/pages/2048Game/NumberTable.js
+++ client/src/components/pages/2048Game/NumberTable.js
@@ -1,17 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./2048Game.css";
 import NumberRow from "./NumberRow";
 
 class NumberTable extends Component {
-    render() {
-        const numbers = this.props.numbers;
-        const numberTableContent = numbers.map((numberRow) =>
-            <NumberRow key={numberRow.id} numberRow={numberRow}/>
-        );
-        return (
-            <div className="number-table">{numberTableContent}</div>
-        );
-    }
+  render() {
+    const numbers = this.props.numbers;
+    const numberTableContent = numbers.map((numberRow, index) => (
+      <NumberRow key={index} numberRow={numberRow} />
+    ));
+    return <div className="number-table">{numberTableContent}</div>;
+  }
 }
 
 export default NumberTable;
