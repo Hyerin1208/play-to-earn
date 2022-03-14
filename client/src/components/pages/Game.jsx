b507c4faf65ae17e5f5629f4ad886743a3ae7d32
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-03-14 12:22:54 +0900

    fix..

--- client/src/components/pages/Game.jsx
+++ client/src/components/pages/Game.jsx
@@ -5,60 +5,57 @@ import SnakeGames from "../pages/SnakeGame/SnakeGame";
 import TetrisGames from "../pages/TetrisGame/Tetris.js";
 import PuzzleGames from "../pages/2048Game/2048Game";
 import MineGames from "../pages/MineGame/MineGame";
-import "./Game.css";
+import "./game.css";
 import CommonSection from "../ui/CommonSection";
 import Ranking from "./Ranking";
 import Compensation from "./Compensation";
 
 function Game() {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-  return (
-    <Fragment>
-      <CommonSection title="GAME'S FEATURES" />
-      <Container>
-        <Row>
-          <Col>
-            <div className="game__infoBox">
-              <button className="ranking__btn">
-                <Link to="/ranking">랭킹 확인</Link>
-              </button>
+    return (
+        <Fragment>
+            <CommonSection title="GAME'S FEATURES" />
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="game__infoBox">
+                            <button className="ranking__btn">
+                                <Link to="/ranking">랭킹 확인</Link>
+                            </button>
 
-              <button
-                className="Compensation__btn"
-                onClick={() => setShowModal(true)}
-              >
-                랭킹별 보상리스트
-              </button>
+                            <button className="Compensation__btn" onClick={() => setShowModal(true)}>
+                                랭킹별 보상리스트
+                            </button>
 
-              {showModal && <Compensation setShowModal={setShowModal} />}
-            </div>
-          </Col>
+                            {showModal && <Compensation setShowModal={setShowModal} />}
+                        </div>
+                    </Col>
 
-          <Col lg="3" md="4" sm="6">
-            <div className="single__game__card">
-              <Link to="SnakeGames">스네이크</Link>
-            </div>
-            <div className="single__game__card">
-              <Link to="TetrisGames">테트리스</Link>
-            </div>
-            <div className="single__game__card">
-            <Link to="PuzzleGames">2048</Link>
-            </div>
-            <div className="single__game__card">
-            <Link to="MineGames">지뢰찾기</Link>
-            </div>
-          </Col>
-        </Row>
-        <Routes>
-        <Route path="SnakeGames" element={<SnakeGames />} />
-        <Route path="TetrisGames" element={<TetrisGames />} />
-        <Route path="PuzzleGames" element={<PuzzleGames />} />
-        <Route path="MineGames" element={<MineGames />} />
-      </Routes>
-      </Container>
-    </Fragment>
-  );
+                    <Col lg="3" md="4" sm="6">
+                        <div className="single__game__card">
+                            <Link to="SnakeGames">스네이크</Link>
+                        </div>
+                        <div className="single__game__card">
+                            <Link to="TetrisGames">테트리스</Link>
+                        </div>
+                        <div className="single__game__card">
+                            <Link to="PuzzleGames">2048</Link>
+                        </div>
+                        <div className="single__game__card">
+                            <Link to="MineGames">지뢰찾기</Link>
+                        </div>
+                    </Col>
+                </Row>
+                <Routes>
+                    <Route path="SnakeGames" element={<SnakeGames />} />
+                    <Route path="TetrisGames" element={<TetrisGames />} />
+                    <Route path="PuzzleGames" element={<PuzzleGames />} />
+                    <Route path="MineGames" element={<MineGames />} />
+                </Routes>
+            </Container>
+        </Fragment>
+    );
 }
 
 export default Game;
