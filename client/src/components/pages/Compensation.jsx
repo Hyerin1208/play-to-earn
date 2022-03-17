103bee44be5f691a411c8a6d79a07d7dd1fe8441
Hyerin1208 <fghjiff8@gmail.com>
2022-03-17 11:19:33 +0900

    reward page css 작업중

--- client/src/components/pages/Compensation.jsx
+++ client/src/components/pages/Compensation.jsx
@@ -9,7 +9,15 @@ const Compensation = ({ setShowModal }) => {
         <span className="close__modal">
           <i className="ri-close-line" onClick={() => setShowModal(false)}></i>
         </span>
-        <h3 className="text-center text-light">랭킹별 보상 리스트</h3>
+        <h2 className="text-center text-light">Weekly Bonus</h2>
+        <div className="reward__content glow">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus ex id saepe voluptatem, quam dolorem itaque illum
+            omnis ut corrupti, porro minus. Qui repellendus corrupti inventore
+            provident, consectetur eveniet labore.
+          </p>
+        </div>
       </div>
     </div>
   );
