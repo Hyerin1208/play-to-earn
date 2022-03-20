10ba554087368050ae4440f57e8e802baa0497f5
Hyerin1208 <fghjiff8@gmail.com>
2022-03-20 15:51:14 +0900

    floating button

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -10,11 +10,13 @@ import Tutorial from "../ui/Tutorial";
 import TokenNomic from "../ui/TokenNomic";
 import AirDrop from "../ui/AirDrop";
 import NewsLetter from "../ui/NewsLetter";
+import SideBar from "../ui/SideBar";
 
 const Home = () => {
   return (
     <>
       <ScrollToTop smooth top="20" />
+      <SideBar />
       <MainSection />
       <AirDrop />
       <LiveList />
