d199969263353ddb296ae5acf68dbcd443e226a6
이성현 <marvel97@naver.com>
2022-03-24 16:34:52 +0900

    게임들 수정중

--- client/src/components/pages/Ranking.jsx
+++ client/src/components/pages/Ranking.jsx
@@ -106,36 +106,56 @@ const Ranking = () => {
                     <br />
                     <p>
                       1st : {snake[0] === undefined ? "없음" : snake[0].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {snake[0] === undefined ? "" : snake[0].snakePoint}
                     </p>
                     <p>
                       2nd : {snake[1] === undefined ? "없음" : snake[1].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {snake[1] === undefined ? "" : snake[1].snakePoint}
                     </p>
                     <p>
                       3rd : {snake[2] === undefined ? "없음" : snake[2].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {snake[2] === undefined ? "" : snake[2].snakePoint}
                     </p>
                     <p>
                       4rd : {snake[3] === undefined ? "없음" : snake[3].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {snake[3] === undefined ? "" : snake[3].snakePoint}
                     </p>
                     <p>
                       5rd : {snake[4] === undefined ? "없음" : snake[4].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {snake[4] === undefined ? "" : snake[4].snakePoint}
                     </p>
                     <br />
                     <b>TetrisGame</b>
                     <br />
                     <p>
                       1st : {tetris[0] === undefined ? "없음" : tetris[0].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {tetris[0] === undefined ? "" : tetris[0].tetrisPoint}
                     </p>
                     <p>
                       2nd : {tetris[1] === undefined ? "없음" : tetris[1].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {tetris[1] === undefined ? "" : tetris[1].tetrisPoint}
                     </p>
                     <p>
                       3rd : {tetris[2] === undefined ? "없음" : tetris[2].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {tetris[2] === undefined ? "" : tetris[2].tetrisPoint}
                     </p>
                     <p>
                       4rd : {tetris[3] === undefined ? "없음" : tetris[3].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {tetris[3] === undefined ? "" : tetris[3].tetrisPoint}
                     </p>
                     <p>
                       5rd : {tetris[4] === undefined ? "없음" : tetris[4].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {tetris[4] === undefined ? "" : tetris[4].tetrisPoint}
                     </p>
 
                     <br />
@@ -143,27 +163,57 @@ const Ranking = () => {
                     <br />
                     <p>
                       1st : {puzzle[0] === undefined ? "없음" : puzzle[0].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {puzzle[0] === undefined ? "" : puzzle[0].puzzlePoint}
                     </p>
                     <p>
                       2nd : {puzzle[1] === undefined ? "없음" : puzzle[1].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {puzzle[1] === undefined ? "" : puzzle[1].puzzlePoint}
                     </p>
                     <p>
                       3rd : {puzzle[2] === undefined ? "없음" : puzzle[2].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {puzzle[2] === undefined ? "" : puzzle[2].puzzlePoint}
                     </p>
                     <p>
                       4rd : {puzzle[3] === undefined ? "없음" : puzzle[3].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {puzzle[3] === undefined ? "" : puzzle[3].puzzlePoint}
                     </p>
                     <p>
                       5rd : {puzzle[4] === undefined ? "없음" : puzzle[4].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {puzzle[4] === undefined ? "" : puzzle[4].puzzlePoint}
                     </p>
                     <br />
-                    <b>MineSweepGame</b>
+                    <b>MineGame</b>
                     <br />
-                    <p>1st : {mine[0] === undefined ? "없음" : mine[0].nick}</p>
-                    <p>2nd : {mine[1] === undefined ? "없음" : mine[1].nick}</p>
-                    <p>3rd : {mine[2] === undefined ? "없음" : mine[2].nick}</p>
-                    <p>4rd : {mine[3] === undefined ? "없음" : mine[3].nick}</p>
-                    <p>5rd : {mine[4] === undefined ? "없음" : mine[4].nick}</p>
+                    <p>
+                      1st : {mine[0] === undefined ? "없음" : mine[0].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {mine[0] === undefined ? "" : mine[0].minePoint}
+                    </p>
+                    <p>
+                      2nd : {mine[1] === undefined ? "없음" : mine[1].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {mine[1] === undefined ? "" : mine[1].minePoint}
+                    </p>
+                    <p>
+                      3rd : {mine[2] === undefined ? "없음" : mine[2].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {mine[2] === undefined ? "" : mine[2].minePoint}
+                    </p>
+                    <p>
+                      4rd : {mine[3] === undefined ? "없음" : mine[3].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {mine[3] === undefined ? "" : mine[3].minePoint}
+                    </p>
+                    <p>
+                      5rd : {mine[4] === undefined ? "없음" : mine[4].nick}
+                      &nbsp;&nbsp;&nbsp;
+                      {mine[4] === undefined ? "" : mine[4].minePoint}
+                    </p>
                   </div>
                 )}
               </div>
