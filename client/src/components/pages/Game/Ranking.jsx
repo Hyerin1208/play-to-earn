0860bf3e470b5c4b8d98173c633ff8bb9bcb1d8a
Hyerin1208 <fghjiff8@gmail.com>
2022-04-03 01:37:21 +0900

    timer OK

--- client/src/components/pages/Game/Ranking.jsx
+++ client/src/components/pages/Game/Ranking.jsx
@@ -15,6 +15,44 @@ const Ranking = () => {
   const [timerMinutes, setTimerMinutes] = useState();
   const [timerSeconds, setTimerSeconds] = useState();
 
+  let interval;
+
+  const startTimer = () => {
+    const countDownDate = new Date("May 01, 2022").getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+
+      const hours = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+      if (distance < 0) {
+        //Stop Timer
+
+        clearInterval(interval.current);
+      } else {
+        // Update Timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
   const toggleTab = (index) => {
     setToggleState(index);
   };
