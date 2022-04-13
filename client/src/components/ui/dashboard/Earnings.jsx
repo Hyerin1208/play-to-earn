78e9a7045170ef2da3763543707be5fe8f4db949
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2022-04-13 21:40:04 +0900

    view mybalance

--- client/src/components/ui/dashboard/Earnings.jsx
+++ client/src/components/ui/dashboard/Earnings.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import "./earning.css";
 
 const Earnings = () => {
-  const [token, setToken] = useState("00.00");
-  return (
-    <div className="earning__card">
-      <div className="card__content">
-        <div className="earning__chart">
-          <i className="ri-copper-diamond-line"></i>
+    const [token, setToken] = useState("00.00");
+    const Mybalance = useSelector((state) => state.AppState.Mybalance);
+    return (
+        <div className="earning__card">
+            <div className="card__content">
+                <div className="earning__chart">
+                    <i className="ri-copper-diamond-line"></i>
+                </div>
+                <div className="earing__text">
+                    <div className="token__mybox">{Mybalance} ETH</div>
+                    <div className="token__mydesc">Total Volume</div>
+                </div>
+            </div>
         </div>
-        <div className="earing__text">
-          <div className="token__mybox">{token} ETH</div>
-          <div className="token__mydesc">Total Volume</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Earnings;
