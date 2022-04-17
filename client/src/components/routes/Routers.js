e4395c119bc13b36948ebf9480e002e1ccf7d267
Hyerin1208 <fghjiff8@gmail.com>
2022-04-17 18:30:54 +0900

    staking

--- client/src/components/routes/Routers.js
+++ client/src/components/routes/Routers.js
@@ -17,6 +17,7 @@ import Error from "../ui/templete/Error404";
 import JoinUs from "../ui/register/JoinUs";
 import Admin from "../pages/Admin/Admin";
 import Evolution from "../pages/Evolution/Evolution";
+import Staking from "../pages/Staking/Staking";
 
 const Routers = () => {
   return (
@@ -35,6 +36,7 @@ const Routers = () => {
         <Route path="/game/*" element={<Game />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/upgrade" element={<Evolution />} />
+        <Route path="/staking" element={<Staking />} />
         <Route path="/test" element={<Test />} />
         <Route path="/error" element={<Error />} />
       </Routes>
