128b7857078c776716c7a02f57e57bca5235baae
Hyerin1208 <fghjiff8@gmail.com>
2022-04-17 22:54:01 +0900

    fix

--- client/src/components/ui/dashboard/MySellList.jsx
+++ client/src/components/ui/dashboard/MySellList.jsx
@@ -84,7 +84,7 @@ const MySellList = () => {
     infinite: false,
   };
 
-  if (Loading) {
+  if (Loading && nftArray[0] === undefined) {
     return (
       <div>
         잠시만 기다려 주세요
@@ -93,7 +93,7 @@ const MySellList = () => {
     );
   } else {
     return (
-      <div className="myselllist__box"></div>
+      <div className="myselllist__box">
         {/* <button onClick={() => mynftlists()}>마이리스트</button> */}
         <div className="slick-arrow">
           <Slider {...settings} style={{ width: 900 }}>
