8ef3cc93029fddfe5f96b4eb2f058156bd1c5a7e
coolmarvel <marvel97@naver.com>
2022-04-19 17:11:56 +0900

    수정완료

--- client/src/components/ui/mainContents/AirDrop.jsx
+++ client/src/components/ui/mainContents/AirDrop.jsx
@@ -3,16 +3,23 @@ import React, { useRef, useState, useEffect } from "react";
 import "./airdrop.css";
 
 const AirDrop = () => {
-  const [timerDays, setTimerDays] = useState("00");
-  const [timerHours, setTimerHours] = useState("00");
-  const [timerMinutes, setTimerMinutes] = useState("00");
-  const [timerSeconds, setTimerSeconds] = useState("00");
+  const [loading, setLoading] = useState(true);
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
   const [isStop, setIsStop] = useState(false);
+  const [defaultTime, setdefaultTime] = useState();
   const timerid = useRef(null);
-  const defaultTime = new Date("apr 29, 2022 18:00:00");
+  const lastDay = new Date("apr 29, 2022 18:00:00").getTime();
+
+  useEffect(async () => {
+    setdefaultTime(lastDay);
+    setLoading(false);
+  }, []);
 
   useEffect(() => {
-    timerid.current = setInterval(async () => {
+    timerid.current = setInterval(() => {
       const countdownDate = new Date(defaultTime).getTime();
 
       const now = new Date().getTime();
@@ -40,38 +47,42 @@ const AirDrop = () => {
 
   return (
     <>
-      <div className="content-wrapper">
-        <h1 className="drop__title">Naming Center NFT Drop Coming Soon!!</h1>
-        <p className="drop__nft" id="new-nft">
-          A new batch of Naming Center nft will be available very soon!
-        </p>
-        <div className="timer">
-          <div className="timer-box">
-            <div className="timer-box__count">
-              <span id="days">{timerDays}</span>
+      {loading ? (
+        <strong> loading... </strong>
+      ) : (
+        <div className="content-wrapper">
+          <h1 className="drop__title">Naming Center NFT Drop Coming Soon!!</h1>
+          <p className="drop__nft" id="new-nft">
+            A new batch of Naming Center nft will be available very soon!
+          </p>
+          <div className="timer">
+            <div className="timer-box">
+              <div className="timer-box__count">
+                <span id="days">{timerDays}</span>
+              </div>
+              <div className="timer-box__text">Days</div>
             </div>
-            <div className="timer-box__text">Days</div>
-          </div>
-          <div className="timer-box">
-            <div className="timer-box__count">
-              <span id="hours">{timerHours}</span>
+            <div className="timer-box">
+              <div className="timer-box__count">
+                <span id="hours">{timerHours}</span>
+              </div>
+              <div className="timer-box__text">Hours</div>
             </div>
-            <div className="timer-box__text">Hours</div>
-          </div>
-          <div className="timer-box">
-            <div className="timer-box__count">
-              <span id="minutes">{timerMinutes}</span>
+            <div className="timer-box">
+              <div className="timer-box__count">
+                <span id="minutes">{timerMinutes}</span>
+              </div>
+              <div className="timer-box__text">Minutes</div>
             </div>
-            <div className="timer-box__text">Minutes</div>
-          </div>
-          <div className="timer-box">
-            <div className="timer-box__count">
-              <span id="seconds">{timerSeconds}</span>
+            <div className="timer-box">
+              <div className="timer-box__count">
+                <span id="seconds">{timerSeconds}</span>
+              </div>
+              <div className="timer-box__text">Seconds</div>
             </div>
-            <div className="timer-box__text">Seconds</div>
           </div>
         </div>
-      </div>
+      )}
 
       <div className="fireworks-container"></div>
     </>
