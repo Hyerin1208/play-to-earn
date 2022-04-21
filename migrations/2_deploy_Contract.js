ed05de82b13c50fdf2c0eb2e2f54010640525299
Hyerin1208 <fghjiff8@gmail.com>
2022-04-21 09:36:34 +0900

    revert

--- migrations/2_deploy_Contract.js
+++
@@ -1,9 +0,0 @@
-var AmusementArcadeToken = artifacts.require("./AmusementArcadeToken.sol");
-var CreateNFT = artifacts.require("./CreateNFT.sol");
-var TokenClaim = artifacts.require("./TokenClaim.sol");
-
-module.exports = async function (deployer) {
-    await deployer.deploy(AmusementArcadeToken);
-    await deployer.deploy(CreateNFT);
-    await deployer.deploy(TokenClaim, AmusementArcadeToken.address, CreateNFT.address);
-};
