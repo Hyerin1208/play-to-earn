830eeaa44b0c9bda2623bea7aee6d7b9eb07ac03
coolmarvel <marvel97@naver.com>
2022-04-26 09:50:46 +0900

    delete all

--- cleint/src/components/pages/TetrisGame/Dropdowns/index.js
+++
@@ -1,5 +0,0 @@
-import ColorsDropdown from './ColorsDropdown';
-import StyleDropdown from './StyleDropdown';
-import LevelDropdown from './LevelDropdown';
-
-export { ColorsDropdown, StyleDropdown, LevelDropdown };
--- cleint/src/components/ui/register/GetChar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GetChar = () => {
-  return <div>GetChar</div>;
-};
-
-export default GetChar;
